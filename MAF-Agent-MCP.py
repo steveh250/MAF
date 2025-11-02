df0281bfc8f66966d842dba1f0551efd3c566f25
Steven Harris <steve.harris@shaw.ca>
2025-11-02 15:37:44 -0800

    Modify PDF path and enhance agent instructions
    Updated the PDF file path and modified agent instructions to include extracting questions and requirements.
    
    Changed  model to use the modified qwen3:8b-40k to deal with the prompt truncation I observed in the log files (see Readme.md for details).

--- MAF-Agent-MCP.py
+++ MAF-Agent-MCP.py
@@ -10,7 +10,7 @@ from agent_framework.openai import OpenAIChatClient, OpenAIAssistantsClient, Ope
 async def run_agent():
     # Initialize the OpenAI chat client (using Ollama endpoint)
     chat_client = OpenAIChatClient(
-        model_id="qwen3:8b",
+        model_id="qwen3:8b-40k",
         api_key="ollama",
         base_url="http://localhost:11434/v1",
     )
@@ -22,20 +22,20 @@ async def run_agent():
         url="http://localhost:8000/mcp",  # MCP server URL
     )
 
-    # Specify the paths to the pdf file
-    pdf_file = "/home/ubuntu/MAF/MAF-Research.pdf"
+    # Specify the paths to the md and pdf file
+    pdf_file = "/home/ubuntu/MAF/Sample-RFP-Managed-Services.pdf"
     print(f"File Name: {pdf_file}\n\n")
-    
+
     # Create the agent with the chat client
     agent = chat_client.create_agent(
         name="PDF Parsing Agent",
-        instructions="You are a helpful assistant that specializes in converting PDFs. You have access to a docling_tool that uses docling to convert documents, you will ask docling to convert the document to a docling format, retrieve the key that identifies the converted document and then ask the docling_tool to convert the file to markdown and then display the markdown for the user.",
+        instructions="You are a helpful assistant that specializes in converting PDFs. You have access to a docling_tool that uses docling to convert documents, you will ask docling to convert the document to a docling format, retrieve the key that identifies the converted document and then ask the docling_tool to convert the file to markdown",
         tools=[docling_tool]
     )
 
     # Run the agent with the MCP tool
     result = await agent.run(
-        f"Using your tools convert the PDF document at {pdf_file} to MarkDown.",
+        f"Using your tools convert the PDF document at {pdf_file} to MarkDown. Then extract the questions and requirements from the RFP that need answers, show me the questions and requirements and suggest how I shoulld respond to each of them by building a response to each section."
     )
 
     print(f"Agent: {result}\n\n")
